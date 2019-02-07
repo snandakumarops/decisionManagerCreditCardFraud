58702c33f2f21e82dee022360c06e08e1e82a3b5
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-02-07 12:15:14 -0500

    Update Main.java

--- src/main/java/com/redhat/demo/dm/ccfraud/Main.java
+++ src/main/java/com/redhat/demo/dm/ccfraud/Main.java
@@ -53,7 +53,7 @@ public class Main {
 
         // creating the consumer using properties config
         Properties config = new Properties();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka.kafka.svc:9092");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
@@ -65,7 +65,7 @@ public class Main {
 
         // subscribe to several topics
         Set<String> topics = new HashSet<>();
-        topics.add("test-vertx");
+        topics.add("events");
 
         consumer.subscribe(topics);
 
